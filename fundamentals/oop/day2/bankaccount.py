62ee1926b811dbc718476608e809f27951cbaa40
Tony Marsee <tonymarseee@gmail.com>
2022-10-01 14:05:50 -0400

    finishedbankaccount

--- fundamentals/oop/day2/bankaccount.py
+++ fundamentals/oop/day2/bankaccount.py
@@ -1,10 +1,22 @@
 class BankAccount:
-    def __init__(self, int_rate, balance): 
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = 0
     def deposit(self, amount):
-        # your code here
+        self.balance = self.balance + amount
+        print(f"Your new balance is $", self.balance, "thank you for your deposit.")
+        return self
     def withdraw(self, amount):
-        # your code here
+        self.balance = self.balance - amount
+        print(f"After your withdraw you have $",self.balance, "remaining, have a nice day.")
+        return self
     def display_account_info(self):
-        # your code here
+        print("Your balance is $",self.balance,"and your interest rate is", self.int_rate)
     def yield_interest(self):
-        # your code here
+        self.balance = self.balance*(1+self.int_rate)
+        return self
+
+account1 = BankAccount(.01, 500)
+account2 = BankAccount(.03, 10000)
+account1.deposit(100).deposit(200).deposit(300).withdraw(250).yield_interest().display_account_info()
+account2.deposit(1250).deposit(1250).withdraw(150).withdraw(75).withdraw(420).withdraw(175).yield_interest().display_account_info()
--- fundamentals/oop/day2/practice/userpractice.py
+++ fundamentals/oop/day2/practice/userpractice.py
@@ -22,6 +22,7 @@ def enroll_user(self):
         self.is_rewards_member = True
         self.gold_card_points = 200
         print("Member has been enrolled, point balance has been set to 200")
+    return self
 
 def spend_points(self,points):
     if self.gold_card_points >= points:
@@ -29,31 +30,10 @@ def spend_points(self,points):
         print(f"Your new point balance is",self.gold_card_points)
     else:
         print(f"Insufficient point balance, you only have",self.gold_card_points)
+    return self
 
-user_Anthony = User("Anthony", "Marsee", "anthonymarsee@gmail.com", 22,False,0)
-user_Kevin = User("Kevin", "Eleven", "kevineleven@gmail.com", 20,False,0)
-user_Nick = User("Nick", "Olodean", "nickolodean@gmail.com", 24,False,0)
+user_1 = User("Anthony", "Marsee", "anthonymarsee@gmail.com", 22,False,0)
+user_2 = User("Kevin", "Eleven", "kevineleven@gmail.com", 20,False,0)
+user_3 = User("Nick", "Olodean", "nickolodean@gmail.com", 24,False,0)
 
-user_info_dump(user_Anthony)
-
-enroll_user(user_Anthony)
-
-user_info_dump(user_Anthony)
-
-user_info_dump(user_Nick)
-
-user_info_dump(user_Kevin)
-
-spend_points(user_Anthony,50)
-
-enroll_user(user_Kevin)
-
-spend_points(user_Kevin,80)
-
-user_info_dump(user_Anthony)
-
-user_info_dump(user_Nick)
-
-enroll_user(user_Anthony)
-
-spend_points(user_Nick,40)
+user_1user_info_dump()
